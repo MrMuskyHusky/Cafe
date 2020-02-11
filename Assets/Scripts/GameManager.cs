efa954cb883ddfa068966ef92bec8059f657da0a
Ethan McGimpsey <54158496+MrMuskyHusky@users.noreply.github.com>
2020-02-11 12:40:37 +1100

    Update #1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOver;
+    public void NextScene()
+    {
+        // Goes straight to the next scene when a level is completed or a trigger is called.
+        Scene activeScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(activeScene.buildIndex + 1);
+    }
+    public void ResetLevel()
+    {
+        // Resets the current level of the scene when you hit a trigger or a ui button is pressed.
+        Scene activeScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(activeScene.buildIndex);
+    }
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void MainMenu(int sceneIndex)
+    {
+        // Loads the scene that has been selected
+        SceneManager.LoadScene(0);
+    }
+    public void OnApplicationQuit()
+    {
+        // Quits the unity application
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts/PickItems.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PickItems : MonoBehaviour
+{
+    public GameObject item;
+    public GameObject tempParent;
+    public Transform guide = null;
+
+    public RaycastHit hit;
+    private Vector3 newPos;
+    private float dist;
+    public float distance = 5;
+
+    private void Start()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (!guide)
+            {
+                if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Food" && Vector3.Distance(Camera.main.transform.position, hit.transform.position) < distance)
+                {
+                    // if it's a rigidbody, zero its physics velocity
+                    if (hit.rigidbody) hit.rigidbody.velocity = Vector3.zero;
+                    guide = hit.transform; // now there's an object picked
+                                             // remember its distance from the camera
+                    dist = Vector3.Distance(guide.position, Camera.main.transform.position);
+                }
+            }
+        }
+    }
+    private void OnMouseDown()
+    {
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        item.transform.position = guide.transform.position;
+        item.transform.rotation = guide.transform.rotation;
+        item.transform.parent = guide.transform.parent;
+    }
+    private void OnMouseUp()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+        item.GetComponent<Rigidbody>().isKinematic = false;
+        item.transform.parent = null;
+        item.transform.position = guide.transform.position;
+    }
+    public void RaycastHit()
+    {
+
+    }
+}
